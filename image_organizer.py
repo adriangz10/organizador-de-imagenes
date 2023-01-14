c6b3d7d83932d79420aaf07afb777b016ba37ee3
adriangz10 <107776079+adriangz10@users.noreply.github.com>
2023-01-13 21:30:30 -0300

    Update and rename ordena_imagenes.py to image_organizer.py

--- image_organizer.py
+++ image_organizer.py
@@ -3,23 +3,26 @@ import shutil
 from datetime import datetime
 
 # Directorio de imagenes
-path = "D:/Variado"
+path = "path/to/images"
 
 # Recorrer cada archivo en el directorio
 for filename in os.listdir(path):
+    
     # Obtener la fecha de creacion del archivo
     file_path = os.path.join(path, filename)
     creation_time = os.path.getctime(file_path)
+    
     # Convertir la fecha a un objeto datetime
     dt_object = datetime.fromtimestamp(creation_time)
+    
     # Extraer el mes y el año del objeto datetime
     month = dt_object.strftime("%B")
     year = dt_object.strftime("%Y")
+    
     # Crear un directorio con el mes y año si no existe
     new_path = os.path.join(path, year, month)
     if not os.path.exists(new_path):
         os.makedirs(new_path)
+        
     # Mover el archivo al directorio correspondiente
     shutil.move(file_path, os.path.join(new_path, filename))
-
-
